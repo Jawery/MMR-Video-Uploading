040776b6ff3b14ab4e841a2db355abf9daeadf3d
estarkweather <emery.starkweather@gmail.com>
2017-03-28 13:51:12 -0500

    Create usersettings.py

---
+++ usersettings.py
@@ -0,0 +1,35 @@
+#IMPORTANT GLOBALS
+
+db_user = "root"
+db_pass = ""
+db_host = "127.0.0.1"
+db_database = "raceinfo"
+
+
+ffprobe = "C:\ffmpeg\bin\ffprobe.exe"
+ffmpeg = "C:\ffmpeg\bin\ffmpeg.exe"
+video_path = 'C:\Users\Emery\Videos\*.mp4'
+ffmpeg_crf = 18
+ffmpeg_preset = "medium"
+
+host = "127.0.0.1"
+port = 12345                # Reserve a port for your service.
+
+attempt = 0
+obs_exe="obs64.exe"
+osb_fgw_name = "OBS 0.16.6 (64bit, windows) - Profile: MMR - Scenes: MMR"
+is_recording = False
+race_class = "no set class"
+race_desc = "not set desc"
+race_name = "not yet set"
+elist = []
+dict_comp = {}
+dict_g = {}
+dict_h = {}
+dict_j = {}
+line_count=0
+
+debug = True
+#debug = False
+
+
