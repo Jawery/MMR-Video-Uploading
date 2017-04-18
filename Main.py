efea905d5f2a17149398b0346734d7874fc4714d
Jawery <jaredmowery@hotmail.com>
2017-04-18 09:27:50 -0600

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,78 +1,108 @@
-import socket
-import time
-import win32com.client
+#!/usr/bin/python           # This is client.py file
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 12345
-BUFFER_SIZE = 1024
-data = ""
-cut_data = ""
-clas = ""
-race = ""
-long_race_name_1 = ""
-long_race_name_2 = ""
-is_recording = False
-time_li = [1,2,3]
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_connect = False
+from usersettings import *
+from mmrfunctions import *
 
-def get_data(): #Retrieves data from TCP server on timing computer in order to be manipulated 
-    
-    datum = "sample"
-    while 1:
-        while tcp_connect == False:
-            try:
-                s.connect((TCP_IP, TCP_PORT))
-                tcp_connect = True
-            except:
-                time.sleep(5)
-        while datum:
-            datum = s.recv(BUFFER_SIZE)
-            if datum:
-                return datum
-        tcp_connect = False
-          
-def set_race_name(): #Sets the new race name and heat in a .txt file which is implemented in OBS
-      
-    f = open("RaceName.txt", "w")
-    f.write(long_race_name_1)
-    f.close()
-    
-def update_li(li): # Updates the list of most recent times in order to determine the end of the race
-  
-    li = cut_data[4] + li
-    del li[-1] #Does this work or does it simply create an instanced copy of time_li---TESTING NEEDED
-      
-def start_recording(): #Opens and virtually presses button to start recording
-  
-    wsh = win32com.client.Dispatch("WScript.Shell")
-    wsh.AppActivate("OpenSource Broadcaster")
-    wsh.SendKeys("F10")
-  
-def stop_recording(): #Opens and virtually presses button to stop recording
+db = Database()                                                                 #create a db object which has a side effect of ing that the db is online 
 
-    wsh = win32com.client.Dispatch("WScript.Shell")
-    wsh.AppActivate("OpenSource Broadcaster")
-    wsh.SendKeys("F10")
-    
-while 1:
-    data = get_data()
-    cut_data = data.split(",")
-    
-    if cut_data[0] == '"$F"':
-        update_li(time_li)  
-        if cut_data[4] == '"0:01"' and is_recording == False:
-            start_recording()
-        elif time_li[0] == time_li[1] == time_li[2] and is_recording == True:
-            stop_recording()
-    
-    
-    elif cut_data[0] == '"$B"':
-        clas = cut_data[1]
-      
-    elif cut_data[0] == '"$C"':
-        race = cut_data[1]
-        long_race_name_1 = race + clas
-        if long_race_name_1 != long_race_name_2:
-            long_race_name_2 = long_race_name_1
-            set_race_name()
+kill_process(obs_exe)
+osb_running()                                                                   #make sure obs is running and in focus.
+
+
+
+print "Program startup at %s" % int(time.time())
+
+while 1:                                                                        #Infinite loop that is always trying to connect to the scoring server
+        s = socket.socket()                                                     #Create socket  
+        attempt+=1                                                              #Create counter for cpnnect attempts
+        print "attempt %s" % attempt                                           #Show which attempt we are on.
+        try:
+                s.connect((host, port))                                         #Attempt to connect to remote server
+                l =  s.recv(1024)                                               #create recieve queue
+               
+                while (l):                                                      #while items are in the queue and we are connected process 
+
+                        line_count+=1
+                        l = s.recv(1024)                                        #set queue to 1024 bytes
+                        if debug: print "raw line ----> %s  <-----" % l
+                        l = l.replace("\"","")                             #strip character from each line recieved      
+                        cut_data = l.split(",")                                 #cut up the comma seperated values sent from scoring server
+                        
+                        if cut_data[0] == "$B":                               #Process data where the first value in the comma seperated vales is $B
+                                race_desc = cut_data[2]                         #extract the race description
+                        
+                        if cut_data[0] == "$C":                               #Process data where the first value in the comma seperated vales is $B
+                              
+                                race_class = cut_data[2]                        #extract the race class
+                                                     
+
+                        if cut_data[0] == "$J":
+                                try:
+                                        dict_j[cut_data[1].replace("\"","")] = {cut_data[1]: { 'last_lap': cut_data[2], 'total_time_elapsed': cut_data[3] }}
+                                                                                
+                                except:
+                                        print "whoops something went wrong processing $J data"
+
+                        if cut_data[0] == "$F":                                                 #Process data where the first value in the comma seperated vales is $F
+                                cut_name = "%s %s" % (race_class.rstrip('\n'),race_desc.rstrip('\n'))
+                                if debug: print "%s in $F routine" % cut_data[2]
+                                elist.insert(0,cut_data[2])                                     #Push latest time left value to elist list
+                                del elist[3:]                                                   #Delete all any elist list elements past the 3rd element
+                                                                                
+                                if stop_test(is_recording):                                                 #Checks to see if the 3 valies for time remaining are all the same if so stop recording 
+                                        print "stop_test() is true will run stop_recording()"
+                                        stop_recording(file)
+                                        clear_dicts()
+                                        print "I ran stop recording"
+                                        is_recording = False                                     
+                                
+                                if cut_data[4] == "0:01":                                   #Has 1 second of the race elapsed
+                                        print "fire start_record()"
+                                        file = start_recording(cut_name)                                #If 1 second has elasped start recording
+                                        is_recording = True
+                                
+                                update_timeleft(cut_data[2])                       #Fire function to update text file with how much time is remaining
+
+                        if cut_data[0] == "$COMP":
+                                
+                                try:
+                                        dict_comp[cut_data[1].replace("\"","")]['number'] = cut_data[2].replace("\"","")
+                                        dict_comp[cut_data[1].replace("\"","")]['first_name'] = cut_data[5].replace("\"","")
+                                        dict_comp[cut_data[1].replace("\"","")]['last_name'] = cut_data[4].replace("\"","")
+                                        dict_comp[cut_data[1].replace("\"","")]['transponder'] = cut_data[3].replace("\"","")
+                                except:
+                                        print "Failed to push new data to comp dictionary"
+                      
+                        if cut_data[0] == "$G":
+
+                                try:
+                                        dict_comp[cut_data[2].replace("\"","")]['position'] = cut_data[1]
+
+                                except:
+                                        print "Failed to push new data to comp dictionary g dictionary"
+                               
+                        cut_name = "%s %s" % (race_class.rstrip('\n'),race_desc.rstrip('\n'))             #set variable to concatination of $B and $C data
+                        set_race_name(cut_name)
+                                             
+
+                print('Connection terminated from server side')                 #If we lose the connection this gets printed
+
+        except:                                                                 #Exception for no connection made
+                print "Server did not respond"                                  #Print out the fact we had the execption
+                time.sleep(1)                                                   #sleep to slow down retries
+s.close                                                                         #close the socket so we can try again
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ mmrfunctions.py
@@ -0,0 +1,299 @@
+from usersettings import *
+import socket               # Import socket module
+import time
+import mysql.connector
+from mysql.connector import errorcode
+import sys
+import win32com.client
+import subprocess
+import glob
+import os
+import urllib2
+import psutil
+
+
+class Database:
+
+    def __init__(self):
+    	try:
+    		self.connection = mysql.connector.connect(user=db_user, password=db_pass,database=db_database)
+        	self.cursor = self.connection.cursor()
+        except mysql.connector.Error as err:
+        	if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+   		    	print "Something is wrong with your user name or password"
+    			print "Program will now exit"
+    			sys.exit(1)
+
+    		elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    			print("Database does not exist")
+    			print "Program will now exit"
+    			sys.exit(1)
+    			print(err)	
+
+
+    def insert(self, query):
+        try:
+            self.cursor.execute(query)
+            self.connection.commit()
+        except:
+            self.connection.rollback()
+
+
+    def update(self, query):
+        try:
+            self.cursor.execute(query)
+            self.connection.commit()
+        except:
+            self.connection.rollback()
+
+
+    def query(self, query):
+    	self.cursor.execute(query)
+        return(self.cursor.fetchall())
+
+
+    def transcode_in_progess(self):
+    	self.cursor.execute("select file from race_data where status = 'recording_done' and  transcode_started is not null and transcode_ended is null")
+    	self.cursor.fetchall()
+    	self.cursor.rowcount
+    	return(int(self.cursor.rowcount))
+
+
+    def next_transcode(self):
+    	self.cursor.execute("select file from race_data where status = 'recording_done' and  transcode_started is null order by id asc limit 1")
+    	return(self.cursor.fetchall())
+
+
+    def upload_in_progess(self):
+    	self.cursor.execute("select file from race_data where status = 'recording_done' and transcode_ended is not null and upload_started is not null and upload_ended is null")
+    	self.cursor.fetchall()
+    	self.cursor.rowcount
+    	return(int(self.cursor.rowcount))
+
+
+    def next_upload(self):
+    	self.cursor.execute("select file from race_data where status = 'recording_done' and  transcode_ended is not null and upload_started is null order by id asc limit 1")
+    	return(self.cursor.fetchall())
+
+
+    def __del__(self):
+    	#self.cursor.close()
+        self.connection.close
+
+
+
+def record_length(file):
+	db=Database()
+	sql = "select * , finished_recording - started_recording as diff from race_data where file = '%s'" % file
+	print sql
+	diff = db.query(sql)
+	
+	#print diff
+	for item in diff:
+		#print type(item)
+		length = int(item[15])
+	sql = "UPDATE race_data SET video_length='%s' WHERE file = '%s'" % (length,file)
+	print sql
+	db.update(sql)
+
+
+def kill_process(process):
+	print "looking for --%s--" % process
+	for proc in psutil.process_iter():
+	   	if proc.name() == process:
+	   		print "match"
+	   		proc.kill()
+
+
+def set_video_duration(file):
+	command = "ffprobe %s 2>&1 |findstr Duration" % file
+	print command
+	#subprocess.call(command, shell=True)
+	process = subprocess.Popen(command, shell=True,
+                           stdout=subprocess.PIPE, 
+                           stderr=subprocess.PIPE)
+
+	# wait for the process to terminate
+	out, err = process.communicate()
+	errcode = process.returncode
+	print "OUTPUT IS %s exit code %s" % (out,errcode)
+	split_data = out.split(",")
+	#print split_data[0]
+	split_data2 = split_data[0].split(":")
+	time = "%s:%s:%s" % (split_data2[1].strip(),split_data2[2],split_data2[3])
+	print "made it here"
+	db = Database()
+	sql = "update race_data set estimated_video_duration = '%s' where file = '%s'" % (time,file.replace("\\","\\\\"))
+	print sql
+	db.update(sql)
+	#return(time) 
+
+def transcode_file(file):
+	command = "ffmpeg %s 2>&1 |findstr Duration" % file
+	#subprocess.call(command, shell=True)
+	process = subprocess.Popen(command, shell=True,
+                           stdout=subprocess.PIPE, 
+                           stderr=subprocess.PIPE)
+
+	# wait for the process to terminate
+	out, err = process.communicate()
+	errcode = process.returncode
+	split_data = out.split(",")
+	#print split_data[0]
+	split_data2 = split_data[0].split(":")
+	time = "%s:%s:%s" % (split_data2[1].strip(),split_data2[2],split_data2[3])
+	return(time) 
+
+
+def osb_running():
+	#### SEE IF WE CAN FIND OSB RUNNING
+
+	shell = win32com.client.Dispatch("WScript.Shell")
+	if not shell.AppActivate(osb_fgw_name):
+	        print "Open Source Broadcaster is not running or can not be brought to the foreground looking for %s \nAttempting to launch OBS" % osb_fgw_name
+	        subprocess.Popen(r'C:\Program Files (x86)\obs-studio\bin\64bit\obs64.exe', cwd=r'C:\Program Files (x86)\obs-studio\bin\64bit')
+	        time.sleep(5)
+	        if shell.AppActivate(osb_fgw_name):
+	                print "I was able to launch OBS and bring %s to the foreground window" % osb_fgw_name
+	        else:
+	                print "ubale to launch obs with focus window check OBS is installed and correct profile is set to default"
+	                sys.exit(1)
+
+	if shell.AppActivate(osb_fgw_name):
+	        print "OSB found with name %s" % osb_fgw_name
+
+
+	### END OBS DETECTION
+
+
+
+def update_comp():
+    f = open("overlay.txt", "w")
+    text=""
+    #print dict_g
+    print "working on overlay\nOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"
+    for num in range(1,15)
+        for key in dict_comp.iterkeys():
+            if dict_comp[key]['position'] == str(num):
+                text = "%s %s: %s %s\n" % (text, key,dict_comp[key]['first_name'],dict_comp[key]['last_name'])
+    
+    print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    #time.sleep(1)
+    print "text is\n%s" % text
+    print "------------ raw dict_comp --------------"
+    print dict_comp
+    print "------------ raw dict_comp --------------"
+    print dict_comp
+    #find_driver("1")  
+    #print dict_g
+
+    f.write(text)
+    f.close()	
+    
+    
+def clear_dicts():
+    dict_comp = {'number' : {}, 'first_name' : {}, 'last_name' : {},'transponder' : {}, 'position' : {}}
+    
+
+def get_number_of_laps():
+        pass
+
+def set_race_name(race_name): #Sets the new race name and heat in a .txt file which is implemented in OBS
+      
+    f = open("RaceName.txt", "w")
+    f.write(race_name.replace("\"",""))
+    f.close()
+    
+def update_li(li): # Updates the list of most recent times in order to determine the end of the race
+  
+    li = cut_data[4] + li
+    del li[-1] #Does this work or does it simply create an instanced copy of time_li---TESTING NEEDED
+
+def latest_video_file():
+	try:
+	        time.sleep(5)
+	        list_of_files = glob.glob(video_path) # * means all if need specific format then *.csv
+	        latest_file = max(list_of_files, key=os.path.getctime)
+	        print latest_file
+	        return(latest_file)
+	except:
+		print "something went wrong with latest_video_file"
+      
+def start_recording(race_name): #Opens and virtually presses button to start recording
+	try:
+		wsh = win32com.client.Dispatch("WScript.Shell")
+		if wsh.AppActivate(osb_fgw_name):
+			time.sleep(0.25)
+			wsh.SendKeys("{F10}")
+			file = latest_video_file()
+        	sql = "insert into race_data (name, started_recording, file,status) values ('%s', '%s', '%s', 'recording')" % (race_name,int(time.time()),file.replace("\\","\\\\"))
+        	print sql
+        	
+        	try:
+        		db = Database()
+        		db.insert(sql)
+        		return(file)
+        	except: 
+        		print "sql shit the bed"
+	except:
+		print "something went wrong in start_record()" 
+    
+
+def stop_recording(file): #Opens and virtually presses button to stop recording
+   
+   wsh = win32com.client.Dispatch("WScript.Shell")
+   if wsh.AppActivate(osb_fgw_name):
+        time.sleep(0.25)
+        wsh.SendKeys("{F11}")
+        print "stopping record"
+        #file = "C:\\placeholder.txt"
+        sql = "UPDATE race_data SET status='recording_done',finished_recording='%s' WHERE file = '%s'" % (int(time.time()),file.replace("\\","\\\\"))
+        print "THIS IS A SQL LOOK HERE ----> %s" % (sql)
+        try:
+        		db = Database()
+        		db.update(sql)
+        		record_length(file.replace("\\","\\\\"))
+        		set_video_duration(file)
+
+        except:
+        	print "stop record sql update is broken"
+
+
+
+def stop_test(is_recording):
+
+        if debug: print "*******\n** %s **\n** %s **\n** %s **" % (len(elist),len(set(elist)),is_recording)
+        if len(elist) == 3 and len(set(elist)) == 1 and is_recording:
+                if debug: print "STOP TEST TRUE"
+                return True
+        else:
+                if debug: print "STOP TEST FALSE"
+                return False
+
+def update_timeleft(timeleft):
+        if debug: print "entered update time"
+        f = open("timeleft.txt", "w")
+        f.write(timeleft.replace("\"",""))
+        f.close()
+
+
+def youtube_online_check(): #Check youtube connectivity
+    try:
+        urllib2.urlopen('http://youtube.com', timeout=1)
+        return True
+    except urllib2.URLError: 
+        return False
+    
+    
+def upload_file(address):
+    #TODO upload file
+    pass
+    
+
+def transcode_file(address):
+    #TODO transcode file
+    pass    
+
+
+
+
